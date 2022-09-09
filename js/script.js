1dc1e029d78747f70eefdf9d648e057a87afb843
Alex <57968617+avgreen14@users.noreply.github.com>
2022-09-09 16:16:14 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const { styler, spring, listen, pointer, value } = window.popmotion;
+
+const ball1 = document.querySelector('img');
+const divStyler1 = styler(ball1);
+const ballXY1 = value({ x: 0, y: 0 }, divStyler1.set);
+
+listen(ball1, 'mousedown touchstart')
+  .start((e) => {
+    e.preventDefault();
+    pointer(ballXY1.get()).start(ballXY1);
+  });
+
+listen(document, 'mouseup touchend')
+  .start(() => {
+    spring({
+      from: ballXY1.get(),
+      velocity: ballXY1.getVelocity(),
+      to: { x: 0, y: 0 },
+      stiffness: 500,
+    }).start(ballXY1);
+  });
